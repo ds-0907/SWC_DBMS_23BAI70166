c097623ab8de11b66c7993604d3959f04180869a
ds-0907 <23bai70166@cuchd.in>
2026-06-23 12:07:27 +0530

    Solution of Distinct Salaries

---
+++ Session 8/Distinct Salaries.sql
@@ -0,0 +1,13 @@
+WITH ranked_salaries AS (
+    SELECT department, salary,
+    DENSE_RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS salary_rank
+    FROM (
+    SELECT DISTINCT department, salary
+    FROM twitter_employee
+    ) temp
+)
+
+SELECT department, salary
+FROM ranked_salaries
+WHERE salary_rank <= 3
+ORDER BY department, salary DESC;
